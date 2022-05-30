3f7141e12fee11ebdc40109c25dea36684afcb7d
Majklzbastlirny <minecz.michal.basler@gmail.com>
2022-05-30 20:16:59 +0200

    Create Main_code.ino

---
+++ Main_code/Main_code.ino
@@ -0,0 +1,21 @@
+//This code gets speed data from GPS and translates it to frequency
+//Made by Michal Basler @Majklzbastlirny
+//Created: 30.5.2022
+//Codename: UR 4.20
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+
+void Loading() {
+  
+}
